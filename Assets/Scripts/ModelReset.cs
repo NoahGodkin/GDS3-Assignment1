49312d765054ba3b38c526225cf351b2d97d5e25
NoahGodkin <noahgodkin040805@outlook.com>
2026-04-23 12:09:50 +1000

    GDS3 Assignment 1
    23/04 upload

--- Assets/Scripts/ModelReset.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class ModelReset : MonoBehaviour
-{
-    public void OnMouseDown()
-    {
-        ReturnButton.currentAnimal.transform.rotation = Quaternion.Euler(0f, -83.698f, 0f);
-    }
-}
