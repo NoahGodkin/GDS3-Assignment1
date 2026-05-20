9829906b683fd9c52298c9e5be1f7a6ce1166a91
NoahGodkin <noahgodkin040805@outlook.com>
2026-05-20 13:23:58 +1000

    20/05 upload

--- Assets/Scripts/ReturnButton.cs
+++ Assets/Scripts/ReturnButton.cs
@@ -24,6 +24,10 @@ public class ReturnButton : MonoBehaviour
 
     public Camera mainCamera;
 
+    public GameObject text1;
+    public GameObject text2;
+    public GameObject text3;
+
     public void Start()
     {
         mainCamera = Camera.main;
@@ -45,7 +49,11 @@ public class ReturnButton : MonoBehaviour
 
         currentRoomChangeCanvas.SetActive(true);
 
-        PenguinInspect.transform.rotation = Quaternion.Euler(0f, -83.698f, 0f);
+        text1.SetActive(false);
+        text2.SetActive(false);
+        text3.SetActive(false);
+
+    PenguinInspect.transform.rotation = Quaternion.Euler(0f, -83.698f, 0f);
 
         PolarBearInspect.transform.rotation = Quaternion.Euler(0f, -83.698f, 0f);
     }
